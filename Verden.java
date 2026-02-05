8d13449aaac37c78b17ea7c98b4796c1bc984caf
Aleksander <Alringstad@outlook.com>
2026-02-05 13:36:44 +0100

    startet på verden

--- Verden.java
+++ Verden.java
@@ -1,7 +1,19 @@
 class Verden{
+    Rutenett rutenett;
+    int genNr;
     public Verden(int rad,int kol){
-        Rutenett rutenett;
-        int genNr = 0;
+        rutenett = new Rutenett(rad,kol);
+        genNr = 0;
+        rutenett.fyllMedTilfeldigeCeller();
+        rutenett.kobleAlleCeller();
+    }
+    public void tegn(){
+        
+        rutenett.tegnRutenett();
+        System.out.println("Generasjon:" + genNr);
+        System.out.print("Antall levende celler " + rutenett.antallLevende());
+    }
+    public void oppdatering(){
         
     }
 }
